5653afec10838793f46290d968bfaa203ba895ee
Kirill <freshjs.ftd@gmail.com>
2021-08-09 16:17:46 +0300

    Add btn example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// нашли кнопки
+const [btn] = document.getElementsByTagName('button');
+
+// создали функцию слушателя
+function btnAlert () {
+  alert('Вы нажали кнопку');
+}
+
+// сказали кнопке что она когда на неё нажимают запускает функцию
+btn.addEventListener('click', btnAlert);
+
