f320639dc578da018b15edb38de0ac29a934d79d
Kirill <freshjs.ftd@gmail.com>
2021-08-13 17:05:36 +0300

    Add element removal example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,53 @@
 'use strict';
-/*
-  есть див и кнопки с цветами
-  по нажатию на кнопки поменять цвет фона дива 
-  на цвет названия кнопки
-  использьовать дата-* аттрибуты при этим
 
+// const root = document.querySelector('#root');
+
+// // 1 создать сам элемент
+// const firstPar = document.createElement('p');
+// // 2 заполнить элемент контентом
+// firstPar.textContent = 'JS Created me';
+
+// const secondPar = document.createElement('p');
+// secondPar.textContent = 'Я буду по середине';
+// // 3 сказать куда именно присоеденить элемент
+// root.append(firstPar, secondPar);
+
+// const thirdPar = document.createElement('p');
+// thirdPar.textContent = 'Я буду последним';
+
+// root.appendChild(thirdPar);
+
+/*
+  создать функцию. которая будет принимать у пользователя текст
+  и будет вставлять новый элемент параграфа с этим текстом 
+  в конец <body>
 */
 
-const btns = document.querySelectorAll('#root > button');
+const classNamesForPar = ['class1', 'createdPar', 'textColor'];
 
-function listener(e) {
-  const {target, target: {dataset : {backgroundColor}}} = e;
-  target.parentElement.style.backgroundColor = backgroundColor;
+function createTextPar(text) {
+  // const userInput = prompt(`Введите текст`);
+  const newPar = document.createElement('p');
+  newPar.textContent = text;
+
+  for (let i = 0; i < classNamesForPar.length; i++) {
+    newPar.classList.add(classNamesForPar[i]);
+  }
+  document.body.append(newPar);
 }
 
-for(const btn of btns) {
-  btn.addEventListener('click', listener);
+// удаление элемента
+// element.remove()
+
+// удаление ребенка родителем
+//parentElement.removeChild(childElement)
+
+
+const closeBtn = document.querySelector('#closeBtn');
+
+function deleteWindow (e) {
+  const { target } = e;
+  target.parentElement.remove();
 }
 
+closeBtn.addEventListener('click', deleteWindow);
