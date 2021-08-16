9b110b383cc0763eb95582bd1b5216046268da8a
Kirill <freshjs.ftd@gmail.com>
2021-08-16 15:26:18 +0300

    Add userCard example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,53 +1,17 @@
 'use strict';
 
-// const root = document.querySelector('#root');
+const cardsContainer = document.querySelector('#root');
 
-// // 1 создать сам элемент
-// const firstPar = document.createElement('p');
-// // 2 заполнить элемент контентом
-// firstPar.textContent = 'JS Created me';
+const userCards = data.map((user) => generateUserCard(user));
+cardsContainer.append(...userCards);
 
-// const secondPar = document.createElement('p');
-// secondPar.textContent = 'Я буду по середине';
-// // 3 сказать куда именно присоеденить элемент
-// root.append(firstPar, secondPar);
+// userObj - обьект с инфой о пользователе
+function generateUserCard (userObj) {
+  const {name, description , profilePicture} = userObj;
 
-// const thirdPar = document.createElement('p');
-// thirdPar.textContent = 'Я буду последним';
+  const userCard = document.createElement();
 
-// root.appendChild(thirdPar);
+  // ваше задание
 
-/*
-  создать функцию. которая будет принимать у пользователя текст
-  и будет вставлять новый элемент параграфа с этим текстом 
-  в конец <body>
-*/
-
-const classNamesForPar = ['class1', 'createdPar', 'textColor'];
-
-function createTextPar(text) {
-  // const userInput = prompt(`Введите текст`);
-  const newPar = document.createElement('p');
-  newPar.textContent = text;
-
-  for (let i = 0; i < classNamesForPar.length; i++) {
-    newPar.classList.add(classNamesForPar[i]);
-  }
-  document.body.append(newPar);
+  return userCard;
 }
-
-// удаление элемента
-// element.remove()
-
-// удаление ребенка родителем
-//parentElement.removeChild(childElement)
-
-
-const closeBtn = document.querySelector('#closeBtn');
-
-function deleteWindow (e) {
-  const { target } = e;
-  target.parentElement.remove();
-}
-
-closeBtn.addEventListener('click', deleteWindow);
