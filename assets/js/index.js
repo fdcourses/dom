014486fff3ca9cbf6b0059f58384f16ee2b0340e
Kirill <freshjs.ftd@gmail.com>
2021-08-25 17:09:23 +0300

    Add form task solution

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,31 @@
 'use strict';
+// есть пустой массив
+// при нажатии на кнопку отображайте текст на странице
+// и записывайте этот текст в массив
+const state = [];
 
-/**
- * Функция сложения чисел
- * @param {number} a первое число
- * @param {number} b второе число
- * @returns {number | Array} сумма чисел
- */
-function sum(a,b) {
-  return a+b;
-}
-sum(a,b);
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); // предотвращает стандартное поведение 
+  
+  const { target : { elements : { text123: {value}}}, target} = e;
+
+  state.push(value);
+
+  const par = document.createElement('p');
+  par.textContent = value;
+  document.body.append(par );
+  
+  target.reset();
+
+});
+
+// document.body.addEventListener('click', (e) => {
+//   e.stopPropagation(); // отвемяет всплытие события
+//   console.dir(e.target);
+// });
+
+// window.addEventListener('click', (e) => {
+//   console.log(e.target);
+// })
