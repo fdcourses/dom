05ae7648b3fe76622432daa3f5cb9383a37359dc
Kirill <freshjs.ftd@gmail.com>
2021-08-26 14:52:33 +0300

    Add repetion

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,23 +2,34 @@
 // есть пустой массив
 // при нажатии на кнопку отображайте текст на странице
 // и записывайте этот текст в массив
+// сбрасывать форму
 const state = [];
 
 const form = document.getElementById('form');
 
+// const listener = ;
+
 form.addEventListener('submit', (e) => {
-  e.preventDefault(); // предотвращает стандартное поведение 
-  
-  const { target : { elements : { text123: {value}}}, target} = e;
+  e.preventDefault();
 
-  state.push(value);
+  const {
+    target: {
+      elements: {
+        text123: { value },
+        text123,
+      },
+    },
+    target,
+  } = e;
 
-  const par = document.createElement('p');
-  par.textContent = value;
-  document.body.append(par );
-  
+  if (value) {
+    console.dir(value);
+    state.push(value);
+
+    document.body.append(value);
+  }
+
   target.reset();
-
 });
 
 // document.body.addEventListener('click', (e) => {
@@ -29,3 +40,20 @@ form.addEventListener('submit', (e) => {
 // window.addEventListener('click', (e) => {
 //   console.log(e.target);
 // })
+
+const object = {
+  value: 10,
+  deepObject: {
+    isTrue: true,
+    isFalse: false,
+  },
+  deepArray: [10, 2, 3],
+};
+
+const {
+  value: number,
+  deepObject: { isTrue },
+  deepObject,
+  deepArray: [first, second, third],
+  deepArray: array,
+} = object;
