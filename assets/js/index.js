fa0c032dbee5727b5686e97559ea5d3fe00ed3a9
Kirill <freshjs.ftd@gmail.com>
2021-08-26 17:09:30 +0300

    Add recursion tasks solution

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,59 +1,108 @@
 'use strict';
-// есть пустой массив
-// при нажатии на кнопку отображайте текст на странице
-// и записывайте этот текст в массив
-// сбрасывать форму
-const state = [];
 
-const form = document.getElementById('form');
+let count = 0;
 
-// const listener = ;
+function test() {
+  count++;
+  if (count < 10) {
+    test();
+  }
+}
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
+// создайте рекурсивную функцию которая принимает положительное число
+// и вызвается пока это число больше 0 и логирует это число
 
-  const {
-    target: {
-      elements: {
-        text123: { value },
-        text123,
-      },
-    },
-    target,
-  } = e;
+/**
+ * Рекурсивно выводит в консоль числа от верхнего предела до 0
+ * @param {number} number
+ */
+function countdown(number) {
+  // debugger;
+  if (number > 0) {
+    console.log(number);
+    countdown(number - 1);
+  }
+}
 
-  if (value) {
-    console.dir(value);
-    state.push(value);
+// создайте функцию logRange принимет два числа
+// и логирует от первого ко второму
+/**
+ *
+ * @param {number} min
+ * @param {number} max
+ */
+function logRange(min, max) {
+  if (min <= max) {
+    console.log(min);
+    logRange(min + 1, max);
+  }
+}
 
-    document.body.append(value);
+/**
+ * Функция рассчета суммы чисел от 1 до max
+ * @param {number} number
+ * @returns {number} сумма чисел от 1 до max
+ */
+function increment(number) {
+  // debugger;
+  let result = number;
+
+  if (number > 0) {
+    // рекурсивные вызовы функции в колл стеке
+    result += increment(number - 1);
   }
 
-  target.reset();
-});
+  // доходим до базы рекурсии
+  return result;
+}
 
-// document.body.addEventListener('click', (e) => {
-//   e.stopPropagation(); // отвемяет всплытие события
-//   console.dir(e.target);
-// });
-
-// window.addEventListener('click', (e) => {
-//   console.log(e.target);
-// })
-
-const object = {
+const tree = {
   value: 10,
-  deepObject: {
-    isTrue: true,
-    isFalse: false,
+  left: {
+    value: 20,
+    left: {
+      value: 10,
+    },
   },
-  deepArray: [10, 2, 3],
+  right: {
+    value: 30,
+  },
 };
 
-const {
-  value: number,
-  deepObject: { isTrue },
-  deepObject,
-  deepArray: [first, second, third],
-  deepArray: array,
-} = object;
+/**
+ *
+ * @param {Object} tree
+ * @param {number} tree.value - значение узла дерева
+ * @param {object} tree.left - левый узел дерева
+ * @param {object} tree.right - правый узел дерева
+ * @returns {number} сумма всех параметров value
+ */
+function getTreeValue({ value, left, right }) {
+  let result = value;
+
+  if (left) {
+    result += getTreeValue(left);
+  }
+
+  if (right) {
+    result += getTreeValue(right);
+  }
+
+  return result;
+}
+
+// написать рекурсивную функцию возведения числа в степень
+/**
+ * @param {number} number число
+ * @param {number} exp степень
+ * @returns {number}
+ */
+function power(number, exp) {
+  if (exp === 0) {
+    return 1;
+  }
+
+  return exp > 0
+    ? number * power(number, exp - 1)
+    : 1 / (number * power(number, Math.abs(exp) - 1));
+}
