e768ad0234b38ec96c36753109582e0a193843b4
Kirill <freshjs.ftd@gmail.com>
2021-08-30 14:11:43 +0300

    Add jsDoc for Stack

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,29 +2,37 @@
 
 class Stack {
   /**
-   * 
    * @param {number} maxSize размер стека
+   * @param {any[]} elements массив вносимых при создании в стек элементов
    */
   constructor(maxSize = 10, ...elements) {
     this.maxSize = maxSize;
     this._size = 0;
 
-    for(let i = 0; i < elements.length; i++) {
+    for (let i = 0; i < elements.length; i++) {
       this.push(elements[i]);
     }
   }
 
+  /**
+   * Внесение в стек предмета
+   * @param {*} item вносимый в стек предмет
+   * @returns {number} размер стека
+   */
   push(item) {
-    if(this._size >= this.maxSize) {
-      throw new RangeError("Stack overflown");
+    if (this._size >= this.maxSize) {
+      throw new RangeError('Stack overflown');
     }
     this[`_${this._size}`] = item;
     this._size++;
     return this._size;
   }
-
+  /**
+   * Метод для удаления значения из стека
+   * @returns {*} удаленное из стека значения
+   */
   pop() {
-    if(this._size === 0 ) {
+    if (this._size === 0) {
       return;
     }
     const deleted = this[`_${this._size - 1}`];
@@ -32,9 +40,12 @@ class Stack {
 
     return deleted;
   }
-
+  /**
+   * Метод для просмотра верхнего значения в стеке
+   * @returns {*} верхнее значение в стеке
+   */
   pick() {
-    return this[`_${this._size - 1}`]
+    return this[`_${this._size - 1}`];
   }
 }
 
