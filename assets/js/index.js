111ced6a93d6cf88cb98c1b407ecd3fc7ed69f91
Kirill <freshjs.ftd@gmail.com>
2021-08-30 16:44:57 +0300

    Add Set

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,80 +1,35 @@
 'use strict';
 
-class Queue {
+const set = new Set();
 
-  constructor(...elements) {
-    this._tail = 0;
-    this._head = 0;
+set.add(1);
 
-    // for(let i = 0; i < elements.length; i++) {
-    //   this.push(elements[i]);
-    // }
+set.add({});
+set.add({});
+set.add({});
 
-    for(const element of elements) {
-      this.push(element);
-    }
-  }
+set.add(2);
+set.add('teste32423423');
+set.add(true);
+set.add([1,2,3]);
 
-  get size() {
-    return this._tail - this._head;
-  }
-  /**
-   * Метод для добавленя элемента в конец очереди
-   * @param {*} item 
-   * @returns {number} размер очереди
-   */
-  push(item) {
-    this[this._tail++] = item;
-    return this.size;
-  }
+set.add('1');
 
-  /**
-   * Метод для удаления элемента с начала очереди
-   * @returns {any} значение удаленного элемента
-   */
-  pop() {
-    if(this.size > 0) {
-      const deleted = this[this._head];
-      delete this[this._head++];
-      return deleted;      
-    }
-  }
-}
-// сделайте функцию которая принимает две очереди
-// и на их основании возвращает одну очередь
-// элементы становятся в новую очередь поочередно
-const q1 = new Queue(1);
-const q2 = new Queue(2,8);
+set.delete(true);
 
-/**
- * Функция которая принимает две очереди и возяращает 
- * очередь с поочередно вставленными данными из принятых очередей
- * @param {Queue} q1 первая очередь
- * @param {Queue} q2 вторая очередь
- * @returns {Queue} очередь из двух очередей
- */
-function mergeQueues (q1,q2) {
-  const resultQueue = new Queue();
+set.has(1);
 
-  while(q1.size || q2.size) {
-    if(q1.size) {
-      resultQueue.push(q1.pop());
-    }
+// for(const entry of set.values()) {
+//   console.log(entry);
+// }
 
-    if(q2.size) {
-      resultQueue.push(q2.pop());
-    }
-  }
+// for(const setEntry of set) {
+//   console.log(setEntry);
+// }
 
-  // for(let i = 0; i !== q1.size + q2.size; ) {
-  //   if(q1.size) {
-  //     resultQueue.push(q1.pop());
-  //   }
+// есть 2 массива
+const arr1 = [1,2,3,4,5];
+const arr2 = [3,4,7,8,9];
 
-  //   if(q2.size) {
-  //     resultQueue.push(q2.pop());
-  //   }
-  // }
-
-  return resultQueue;
-}
+//получить массив с уникальными значениями из двух верхних массивов
+const unique = [...new Set([...arr1, ...arr2 ])];
