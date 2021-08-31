900e9762c76d328190f6273a6669b79d728547f8
Kirill <freshjs.ftd@gmail.com>
2021-08-31 16:25:22 +0300

    Add msgToUser task

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,74 +1,32 @@
 'use strict';
 
-const user = {
-  id: 2,
-};
-
-const obj = {
+const u1 = {
   id: 1,
-  name: 'Test',
-  lastName: 'Test',
-  id: 1000,
-  age: 5,
-  0: false,
-  [user]: 'Hello',
-};
+  name: 'John',
+  age: 20
+}
 
-const objEntries = Object.entries(obj);
-const objKeys = Object.keys(obj);
-const objValue = Object.values(obj);
-const objSize = objValue.length;
+const u2 = {
+  id: 2,
+  name: 'Jane',
+  age: 23
+}
 
-const map = new Map();
+const johnMsg = [
+  'Hello',
+  'yes',
+  'no',
+  'test'
+]
 
-const arr = [1, 34, 5];
+const janeMsg = [
+  'Hi',
+  'abracadabra?',
+  'test',
+  'not test'
+]
 
-const func = () => {};
+// создайте функцию, которая будет принимать айдишник пользователя
+// и будет возвращать все его сообщения
 
-map.set('id', 1);
-map.set(arr, 'array');
-map.set(func, 'function');
-map.set(1, 1);
-map.set('1', 'number one');
-map.set(user, 'object');
-
-map.set(map, 'lol');
-
-const value = map.get('1');
-map.delete('1');
-// map.clear();
-
-const vocabulary = new Map([
-  ['получить', 'obtain'],
-  ['преимущество', 'advantage'],
-  ['стул', 'chair'],
-  ['наличка', 'cash'],
-  ['цифробуквенный', 'alphanumerical'],
-  ['переговоры', 'negotiacions'],
-  ['значительный', 'significant'],
-  ['кот', 'cat'],
-  ['собака', 'dog'],
-  ['слон', 'elephant'],
-  ['язык', 'language'],
-]);
-
-const stringToTranslate =
-  'пульверизатор слон значительный получить наличка собака кот цифробуквенный стул';
-
-const string = 'test|not|yet|hello|there|кот';
-const arrFromStr = string.split('|');
-const reformedStr = arrFromStr.join('$');
-
-// создать функцию которая принимает строку на руссокм
-// и возвращает строку на английском
-const translate = (str) =>
-  str
-    .split(' ')
-    .map((key) => (vocabulary.has(key) ? vocabulary.get(key) : key))
-    .join(' ');
-
-// if (vocabulary.has(key)) {
-//   return ;
-// } else {
-//   return ;
-// }
+const msgToUsers = new Map();
