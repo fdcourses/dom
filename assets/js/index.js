16eb99032a171273a527c8e2240947e06f795f45
Kirill <freshjs.ftd@gmail.com>
2021-09-06 16:45:46 +0300

    Refactor index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,87 +13,44 @@ cardsContainer.append(...userCards);
  * @returns {HTMLLIElement} верстка карточки
  */
 function generateUserCard(userObj) {
-  const { id, firstName, description, profilePicture } = userObj;
-
-  const img = createElement('img', {
-    classNames: ['img'],
-    attrs: { src: profilePicture, alt: firstName , 'data-id' : id},
-  });
-  img.addEventListener('error', deleteHandler);
-  img.addEventListener('load', imageLoadHandler);
+  const { firstName, description="test", contacts } = userObj;
 
   const userName = createElement(
     'h2',
-    { classNames: ['cardName'] },
+    { classNamesArray: ['cardName'] },
     document.createTextNode(firstName)
   );
 
   const cardDescription = createElement(
     'p',
     {
-      classNames: ['cardDescription'],
+      classNamesArray: ['cardDescription'],
     },
     document.createTextNode(description)
   );
 
-  const initails = createElement(
-    'div',
-    { classNames: ['initials'] },
-    document.createTextNode(
-      firstName
-        .trim()
-        .split(' ')
-        .map((word) => word[0])
-        .join(' ')
-    )
-  );
-  initails.style.backgroundColor = stringToColour(firstName);
+  const imgWrapper = createImageWrapper(userObj);
 
-  const imgWrapper = createElement(
+  const linkWrapper = createElement(
     'div',
-    { classNames: ['imgWrapper'], attrs: {id: `wrapper${id}`} },
-    initails
+    { classNamesArray: ['linkWrapper'] },
+    ...generateLinks(contacts)
   );
 
   const article = createElement(
     'article',
-    { classNames: ['userCard'] },
+    { classNamesArray: ['userCard'] },
     imgWrapper,
     userName,
-    cardDescription
+    cardDescription,
+    linkWrapper
   );
 
   const userCard = createElement(
     'li',
-    { classNames: ['cardWrapper'] },
+    { classNamesArray: ['cardWrapper'] },
     article
   );
 
   return userCard;
-}
-
-/*
-  UTILS
-*/
-/**
- * Функция генерации цвета для строки
- * @param {string} str 
- * @returns {string} строка в виде хекс-кода (#FF3610)
- */
-function stringToColour(str) {
-  let hash = 0;
-  for (let i = 0; i < str.length; i++) {
-    hash = str.charCodeAt(i) + ((hash << 5) - hash);
-  }
-  let colour = '#';
-  for (let i = 0; i < 3; i++) {
-    let value = (hash >> (i * 8)) & 0xff;
-    colour += ('00' + value.toString(16)).substr(-2);
-  }
-  return colour;
-}
-
-function generateLinks(contacts) {
-  // пройтись мапом выернуть уже готовые элементы ссылок
-  // воспользоватся мапой для определния какую картинку делать
 }
