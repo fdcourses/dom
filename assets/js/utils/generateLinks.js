0a66071b1678bea66df24a718fd3a6d9786742ac
Kirill <freshjs.ftd@gmail.com>
2021-09-06 16:45:08 +0300

    Add generateLinks

---
+++ assets/js/utils/generateLinks.js
@@ -0,0 +1,23 @@
+/**
+ * Генерирует массив ссылок на соц сетки на основании контактов
+ * @param {string[]} contacts ссылки на соц сети
+ * @returns {HTMLAnchorElement[]} массив с элементами ссылок
+ */
+ function generateLinks(contacts) {
+  // пройтись мапом выернуть уже готовые элементы ссылок
+  // воспользоватся мапой для определния какую картинку делать
+  const linksArray = contacts.map((contact) => {
+    const url = new URL(contact);
+    const hostname = url.hostname;
+
+    if (SUPPORTED_SOCIAL_NETWORKS.has(hostname)) {
+      const link = createElement('a', {
+        classNamesArray: SUPPORTED_SOCIAL_NETWORKS.get(hostname),
+      });
+
+      return link;
+    }
+  }).filter(Boolean);
+  console.log(linksArray)
+  return linksArray;
+}
